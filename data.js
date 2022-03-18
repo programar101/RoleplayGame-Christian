f1d6415de76517295ad919482de866599bb0ee16
Christian Carballo Madrigal <91039604+CarballoChr@users.noreply.github.com>
2022-03-17 23:00:44 -0300

    Add files via upload
    Básicamente es lo mismo que está en Scrimba pero modifiqué los avatares. Cuando termine el modulo 6 le quiero agregar algunas cosas que tengo pensadas

---
+++ data.js
@@ -0,0 +1,22 @@
+const characterData = {
+    hero: {
+        name: "Pato",
+        avatar: "https://image.shutterstock.com/image-photo/colourful-mallard-duck-isolated-on-260nw-1710007546.jpg",
+        health: 100,
+        diceCount: 3,
+        currentDiceScore: [],
+        victoryPic: "https://cimsec.org/wp-content/uploads/2015/12/Sauron.png"
+    },
+    monster: {
+        name: "Sauron",
+        avatar: "https://p4.wallpaperbetter.com/wallpaper/502/751/721/sauron-the-lord-of-the-rings-fantasy-art-hd-wallpaper-preview.jpg",
+        health: 50,
+        diceCount: 1,
+        currentDiceScore: [],
+        victoryPic: "https://cdn.leitesculinaria.com/wp-content/uploads/fly-images/238861/spit-roasted-duck-orange-rosemary-1200x900-c.jpg.optimal.jpg"
+    }
+}
+
+
+
+export default characterData
