1b66ae6b72b1e3a75eb6ee9dfd102369926f272d
Christian Carballo <carballo.chr@gmail.com>
2022-03-24 18:00:46 -0300

    Cambios

--- data.js
+++ data.js
@@ -1,22 +1,33 @@
 const characterData = {
     hero: {
-        name: "Pato",
+        name: "Wizard",
         avatar: "https://image.shutterstock.com/image-photo/colourful-mallard-duck-isolated-on-260nw-1710007546.jpg",
-        health: 100,
+        health: 60,
         diceCount: 3,
-        currentDiceScore: [],
-        victoryPic: "https://cimsec.org/wp-content/uploads/2015/12/Sauron.png"
+        currentDiceScore: []
     },
-    monster: {
-        name: "Sauron",
+    orc: {
+        name: "Orc",
         avatar: "https://p4.wallpaperbetter.com/wallpaper/502/751/721/sauron-the-lord-of-the-rings-fantasy-art-hd-wallpaper-preview.jpg",
-        health: 50,
+        health: 30,
         diceCount: 1,
         currentDiceScore: [],
         victoryPic: "https://cdn.leitesculinaria.com/wp-content/uploads/fly-images/238861/spit-roasted-duck-orange-rosemary-1200x900-c.jpg.optimal.jpg"
+    },
+    demon: {
+        name: "Demon",
+        avatar: "https://img2.freepng.es/20180404/blq/kisspng-god-of-war-iii-god-of-war-ascension-playstation-2-god-of-war-5ac48e45770a92.1823339815228309174876.jpg",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "https://upload.wikimedia.org/wikipedia/commons/6/62/Cthulhu_and_R%27lyeh.jpg",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
     }
 }
-
-
 
 export default characterData
